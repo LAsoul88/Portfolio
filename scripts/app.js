bd254e8c9bdc7b9bea815c17005922a2b67c1799
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-10-02 21:00:07 -0700

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+const applications = [
+  {
+    name: 'Album Love',
+    link: 'https://album-love.herokuapp.com/albums',
+    image: 'images/Album-Love.png',
+    description: '',
+  },
+  {
+    name: 'Wayfarer',
+    link: 'https://the-number-one-wayfarer.herokuapp.com/',
+    image: 'images/Wayfarer.png',
+    description: '',
+  },
+  {
+    name: 'Quelue',
+    link: 'https://quelue.herokuapp.com/login',
+    image: 'images/Quelue.png',
+    description: '',
+  },
+  {
+    name: 'frienDroid',
+    link: 'https://lasoul88.github.io/Tomagotchi/',
+    image: 'images/frienDroid.png',
+    description: '',
+  }
+]
